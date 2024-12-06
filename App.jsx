4301aef458b06cbaf807d92f719a86f0791eeec3
tachyglossues <81535731+tachyglossues@users.noreply.github.com>
2024-12-06 02:34:33 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,11 +4,10 @@ import PropTypes from 'prop-types';
 
 const App = () => {
     const [weatherData, setWeatherData] = useState(null);
-    const [selectedCity, setSelectedCity] = useState(null);
     const [cities, setCities] = useState([]);
 
     useEffect(() => {
-        fetchData('castre');
+        fetchData('Albi');
     }, []);
 
     const handleSearch = async (searchTerm) => {
@@ -24,7 +23,6 @@ const App = () => {
 
     const fetchData = async (searchTerm) => {
         try {
-
             const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=ff191ed1cbe746f8bf1180015240512&q=${searchTerm}&days=7&aqi=no&alerts=no`);
             setWeatherData(response.data);
         } catch (error) {
@@ -34,7 +32,6 @@ const App = () => {
 
     const handleCityClick = (cityName, cityRegion) => {
         const selectedCityInfo = cities.find(city => city.name === cityName && city.region === cityRegion);
-        setSelectedCity(selectedCityInfo);
         handleSearch(`${cityName} ${selectedCityInfo.region}`);
         fetchData(`${cityName} ${selectedCityInfo.region}`);
     };
@@ -44,55 +41,60 @@ const App = () => {
             padding: '80px',
             backgroundColor: '#304687',
             display: 'flex',
-            position: 'fixed',
             flexDirection: 'column',
             zIndex: 0
         }}>
             <BlocGlobalGauche weatherData={weatherData} />
             <BlocGlobalDroit
-                weatherData={weatherData}
                 handleSearch={handleSearch}
                 cities={cities}
                 handleCityClick={handleCityClick}
             />
 
             <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
+                position: 'fixed',
+                bottom: 0,
+                left: 0,
+                right: 0,
                 height: '39vh',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                borderTop: '4px solid white',
+                backgroundColor: '#4A67BF'
             }}>
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'flex-start',
-                    gap: '20px',
-                    zIndex: 1,
-                }}>
-                    {Array(7).fill(null).map((_, index) => (
-                        <BlocsMeteo
-                            key={index}
-                            weatherData={weatherData}
-                            index={index}
-                        />
-                    ))}
-                </div>
+                {Array(7).fill(null).map((_, index) => (
+                    <BlocsMeteo
+                        key={index}
+                        weatherData={weatherData}
+                        index={index}
+                    />
+                ))}
             </div>
         </div>
     );
-
-
-
 };
 
 const BlocGlobalGauche = ({ weatherData }) => {
     return (
         <div style={{
-            position: 'fixed', top: 65, left: 10, width: '49%', height: '60%', borderRadius: '40px', backgroundColor: '#4A67BF', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+            position: 'fixed',
+            top: 65,
+            left: 10,
+            width: '49%',
+            height: '60%',
+            borderRadius: '40px',
+            backgroundColor: '#4A67BF',
+            color: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
-            <h1> {weatherData?.location.name}, {weatherData?.location.region}, {weatherData?.location.country}</h1>
-            <img src={weatherData?.current.icon} alt="Icone météo" />
+            <h1>{weatherData?.location.name}, {weatherData?.location.region}, {weatherData?.location.country}</h1>
+            <p>{weatherData?.current.temp_c}</p>
+            <img src={`https:${weatherData?.current.condition.icon}`} alt="Icone météo" />
         </div>
     )
 }
@@ -101,8 +103,7 @@ BlocGlobalGauche.propTypes = {
     weatherData: PropTypes.object
 };
 
-
-const BlocGlobalDroit = ({ weatherData, handleSearch, cities, handleCityClick }) => {
+const BlocGlobalDroit = ({ handleSearch, cities, handleCityClick }) => {
     return (
         <div style={{
             position: 'fixed',
@@ -152,17 +153,18 @@ const BlocGlobalDroit = ({ weatherData, handleSearch, cities, handleCityClick })
 }
 
 BlocGlobalDroit.propTypes = {
-    weatherData: PropTypes.object,
     handleSearch: PropTypes.func.isRequired,
     cities: PropTypes.arrayOf(PropTypes.object),
     handleCityClick: PropTypes.func.isRequired
 };
 
-
-
 const BlocsMeteo = ({ weatherData, index }) => {
     const dayData = weatherData?.forecast.forecastday[index];
-    console.log(dayData);
+
+    if (!dayData || !dayData.day || !dayData.day.condition) {
+        return <div>No data available for this day</div>;
+    }
+
     return (
         <div style={{
             display: 'flex',
@@ -175,18 +177,19 @@ const BlocsMeteo = ({ weatherData, index }) => {
             borderRadius: '30px',
             backgroundColor: '#4A67BF'
         }}>
-            <p> JOUR {index + 1} </p>
-            <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-            <p>{dayData?.day.avgtemp_c}°C</p>
+            <p>JOUR {index + 1}</p>
+            <img
+                src={`https:${dayData.day.condition.icon}`}
+                alt={`Condition météo pour jour ${index + 1}`}
+            />
+            <p>{dayData.day.avgtemp_c}°C</p>
         </div>
     );
 };
-
 
 BlocsMeteo.propTypes = {
     weatherData: PropTypes.object,
     index: PropTypes.number.isRequired
 };
-
 
 export default App;
