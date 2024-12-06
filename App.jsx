744f63f629eafc79562cd9b465f1b227a7f7b3a2
tachyglossues <81535731+tachyglossues@users.noreply.github.com>
2024-12-06 01:51:23 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,187 +1,192 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
-const BlocPleinEcran = () => 
-{
-  return (
-    <div style={{
-      position: 'fixed', top : 50, left: 0, width: '100%', height: '75%', borderBottomLeftRadius: '45px', borderBottomRightRadius: '45px', backgroundColor: '#304688', color: 'white', display: 'flex', flexDirection : 'column', justifyContent: 'center', alignItems: 'center' }}>
+const App = () => {
+    const [weatherData, setWeatherData] = useState(null);
+    const [selectedCity, setSelectedCity] = useState(null);
+    const [cities, setCities] = useState([]);
 
-      <BlocGlobalGauche />
-      <BlocGlobalDroit />
-    </div>
-  );
+    useEffect(() => {
+        fetchData('castre');
+    }, []);
+
+    const handleSearch = async (searchTerm) => {
+        try {
+            const response = await axios.get(
+                `https://api.weatherapi.com/v1/search.json?key=ff191ed1cbe746f8bf1180015240512&q=${searchTerm}`
+            );
+            setCities(response.data);
+        } catch (error) {
+            console.error('Erreur:', error);
+        }
+    };
+
+    const fetchData = async (searchTerm) => {
+        try {
+
+            const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=ff191ed1cbe746f8bf1180015240512&q=${searchTerm}&days=7&aqi=no&alerts=no`);
+            setWeatherData(response.data);
+        } catch (error) {
+            console.error('Erreur:', error);
+        }
+    };
+
+    const handleCityClick = (cityName, cityRegion) => {
+        const selectedCityInfo = cities.find(city => city.name === cityName && city.region === cityRegion);
+        setSelectedCity(selectedCityInfo);
+        handleSearch(`${cityName} ${selectedCityInfo.region}`);
+        fetchData(`${cityName} ${selectedCityInfo.region}`);
+    };
+
+    return (
+        <div style={{
+            padding: '80px',
+            backgroundColor: '#304687',
+            display: 'flex',
+            position: 'fixed',
+            flexDirection: 'column',
+            zIndex: 0
+        }}>
+            <BlocGlobalGauche weatherData={weatherData} />
+            <BlocGlobalDroit
+                weatherData={weatherData}
+                handleSearch={handleSearch}
+                cities={cities}
+                handleCityClick={handleCityClick}
+            />
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+                height: '39vh',
+            }}>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'flex-start',
+                    gap: '20px',
+                    zIndex: 1,
+                }}>
+                    {Array(7).fill(null).map((_, index) => (
+                        <BlocsMeteo
+                            key={index}
+                            weatherData={weatherData}
+                            index={index}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+
+
+
 };
 
-const BlocGlobalGauche = () =>
-{
-  return (
-    <div style={{
-      position : 'fixed', top : 65, left : 10, width: '49%', height: '60%', borderRadius: '40px', backgroundColor: '#4A67BF', color: 'white', display: 'flex', flexDirection : 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <h1> Albi, Tarn, France</h1>
-      <img src="https://via.placeholder.com/180" alt="Exemple d'image" />
-    </div>
-  )
+const BlocGlobalGauche = ({ weatherData }) => {
+    return (
+        <div style={{
+            position: 'fixed', top: 65, left: 10, width: '49%', height: '60%', borderRadius: '40px', backgroundColor: '#4A67BF', color: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+        }}>
+            <h1> {weatherData?.location.name}, {weatherData?.location.region}, {weatherData?.location.country}</h1>
+            <img src={weatherData?.current.icon} alt="Icone météo" />
+        </div>
+    )
 }
 
-const BlocGlobalDroit = () =>
-  {
+BlocGlobalGauche.propTypes = {
+    weatherData: PropTypes.object
+};
+
+
+const BlocGlobalDroit = ({ weatherData, handleSearch, cities, handleCityClick }) => {
     return (
-      <div style={{
-        position : 'fixed', top : 65, right : 10, width: '49%', height: '60%', borderRadius: '40px', backgroundColor: '#4A67BF', color: 'white', display: 'flex', flexDirection : 'column', justifyContent: 'center', alignItems: 'center' }}>
-      </div>
+        <div style={{
+            position: 'fixed',
+            top: 65,
+            right: 10,
+            width: '49%',
+            height: '60%',
+            borderRadius: '40px',
+            backgroundColor: '#4A67BF',
+            color: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <input
+                type="text"
+                placeholder="Rechercher une ville..."
+                onChange={(e) => handleSearch(e.target.value)}
+                style={{
+                    width: '80%',
+                    padding: '10px',
+                    fontSize: '16px',
+                    marginBottom: '20px'
+                }}
+            />
+
+            {cities.length > 0 && (
+                <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+                    {cities.map((city) => (
+                        <li
+                            key={city.id}
+                            onClick={() => handleCityClick(city.name, city.region)}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '5px',
+                                borderBottom: '1px solid white'
+                            }}
+                        >
+                            {city.name}, {city.region}, {city.country}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     )
-  }
+}
 
-const BlocsMeteoLundi = () => 
-{
-  return (
-    <div style={{
-      display: 'flex',
-      flexDirection : 'column',
-      justifyContent: 'center',
-      alignItems : 'center',
-      textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-      <p> LUNDI </p>
-      <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-      <p>Voici le second texte du bloc, après l'image.</p>
-    </div>
-  );
+BlocGlobalDroit.propTypes = {
+    weatherData: PropTypes.object,
+    handleSearch: PropTypes.func.isRequired,
+    cities: PropTypes.arrayOf(PropTypes.object),
+    handleCityClick: PropTypes.func.isRequired
 };
 
-const BlocsMeteoMardi = () => 
-{
+
+
+const BlocsMeteo = ({ weatherData, index }) => {
+    const dayData = weatherData?.forecast.forecastday[index];
+    console.log(dayData);
     return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> MARDI </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            padding: '20px',
+            border: '3px solid #000000',
+            borderRadius: '30px',
+            backgroundColor: '#4A67BF'
+        }}>
+            <p> JOUR {index + 1} </p>
+            <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
+            <p>{dayData?.day.avgtemp_c}°C</p>
+        </div>
     );
 };
 
-const BlocsMeteoMercredi = () => 
-{
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> MERCREDI </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
-    );
+
+BlocsMeteo.propTypes = {
+    weatherData: PropTypes.object,
+    index: PropTypes.number.isRequired
 };
 
-const BlocsMeteoJeudi = () => 
-{
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> JEUDI </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
-    );
-};
-
-const BlocsMeteoVendredi = () => 
-{
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> VENDREDI </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
-    );
-};
-
-const BlocsMeteoSamedi = () => 
-{
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> SAMEDI </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
-    );
-};
-
-const BlocsMeteoDimanche = () => 
-{
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection : 'column',
-        justifyContent: 'center',
-        alignItems : 'center',
-        textAlign: 'center', padding: '20px', border: '3px solid #000000', borderRadius: '30px', backgroundColor: '#4A67BF' }}>
-        <p> DIMANCHE </p>
-        <img src="https://via.placeholder.com/120" alt="Exemple d'image" />
-        <p>Voici le second texte du bloc, après l'image.</p>
-      </div>
-    );
-};
-
-const App = () => 
-{
-  return (
-    <div style={{
-      padding: '80px',
-      backgroundColor : '#304687',
-      display: 'flex',
-      position: 'fixed',
-      flexDirection: 'column',
-      zIndex : 0
-    }}>
-      <BlocPleinEcran />
-
-    <div style={{
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    height: '39vh',
-  }}>
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'flex-start',
-      gap: '20px',
-      zIndex: 1,
-    }}>
-      <BlocsMeteoLundi />
-      <BlocsMeteoMardi />
-      <BlocsMeteoMercredi />
-      <BlocsMeteoJeudi />
-      <BlocsMeteoVendredi />
-      <BlocsMeteoSamedi />
-      <BlocsMeteoDimanche />
-    </div>
-  </div>
-  </div>
-  );
-};
 
 export default App;
